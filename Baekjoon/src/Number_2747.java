331c2a98b13208c4047c17685f07fd2a9124f34d
kdh30806 <kdh30806@naver.com>
2023-04-29 20:50:32 +0900

    2747

--- Baekjoon/src/Number_2747.java
+++ Baekjoon/src/Number_2747.java
@@ -6,35 +6,41 @@ import java.io.OutputStreamWriter;
 import java.util.Arrays;
 
 public class Number_2747 {
-
-	static int[] N_array;
 	
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		int N = Integer.parseInt(br.readLine());
-		N_array = new int[N + 1];
-		Arrays.fill(N_array, -1);
-		N_array[0] = 0;
-		N_array[1] = 1;
-		bw.write(String.valueOf(Fibonacci(N)));
+		String str = "";
 		
+		while(!((str = br.readLine()).equals("0"))) {
+			int center = str.length() / 2;
+			String front_str = "";
+			String back_str = "";
+			if(str.length() % 2 == 0) {
+				front_str = str.substring(0, center);
+				back_str = str.substring(center);
+			}else {
+				front_str = str.substring(0, center);
+				back_str = str.substring(center + 1);
+			}
+			
+			String make_str = "";
+			for(int i = front_str.length() - 1 ; i >= 0 ; i--) {
+				make_str += String.valueOf(front_str.charAt(i));
+			}
+			
+			if(make_str.equals(back_str)) {
+				bw.write("yes" + "\n");
+			}else {
+				bw.write("no" + "\n");
+			}		
+		}
+
 		br.close();
 		bw.flush();
 		bw.close();
-	}
-	
-	public static int Fibonacci(int N) {
-	
-		if(N_array[N] != -1) {
-			return N_array[N];
-		}else {
-			return N_array[N] = Fibonacci(N - 1) + Fibonacci(N - 2);
-		}
-	
-		
 	}
 
 }
